83ddf5079e75d521ee418ea1606a32e7509ff775
Elango <elango.info93@gmail.com>
2024-11-03 23:01:24 +0530

    bjhb

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import { API_URL, DEFAULT_API_KEY } from '@env'
 
 // Create a .env file and add API_URL
-export const api_url = "https://coconut-api.onrender.com";
+export const api_url = "https://coconut-api-k4jf.onrender.com";
 
 export const defaultAPIKey = DEFAULT_API_KEY
